b109cf2fe926a1bba5c6a76a93e9cc804c681483
Ron Diamond <rondiamond@users.noreply.github.com>
2018-08-25 01:24:54 -0700

    created new Xcode project (in Xcode 10.0 beta 6), using Framework template (with unit tests)

---
+++ Reduxion-iOS/Reduxion-iOS/Reduxion_iOS.h
@@ -0,0 +1,19 @@
+//
+//  Reduxion_iOS.h
+//  Reduxion-iOS
+//
+//  Created by Ron Diamond on 8/25/18.
+//  Copyright © 2018 Ron Diamond. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Reduxion_iOS.
+FOUNDATION_EXPORT double Reduxion_iOSVersionNumber;
+
+//! Project version string for Reduxion_iOS.
+FOUNDATION_EXPORT const unsigned char Reduxion_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Reduxion_iOS/PublicHeader.h>
+
+
---
+++ Reduxion-iOS/Reduxion-iOSTests/Reduxion_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  Reduxion_iOSTests.swift
+//  Reduxion-iOSTests
+//
+//  Created by Ron Diamond on 8/25/18.
+//  Copyright © 2018 Ron Diamond. All rights reserved.
+//
+
+import XCTest
+@testable import Reduxion_iOS
+
+class Reduxion_iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
