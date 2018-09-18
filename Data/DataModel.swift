9e318c6425b5cf451d8b9c29431ec57aa51b524b
Ron Diamond <rondiamond@users.noreply.github.com>
2018-09-17 19:08:24 -0700

    data model tweaks

--- Data/DataModel.swift
+++ Data/DataModel.swift
@@ -3,13 +3,21 @@ import Foundation
 // MARK: - Data model
 
 /**
- *  Operands inputted by the user, for doing calculations.
+ * A
+ Operands inputted by the user, for doing calculations.
+ */
+
+/**
+ An arithmetic calculation to be done, consisting of:
+ -  Two operands (floats)
+ -  A type of calculation (e.g., addition, subtraction, etc.)
+ -  A timestamp of when the calculation was requested
  */
 struct Calculation {
-    var operand1: Int
-    var operand2: Int
+    var operand1: Float
+    var operand2: Float
     var calculationType: CalculationType
-    var time: Date
+    var timestamp: Date
 }
 
 struct Calculations {
