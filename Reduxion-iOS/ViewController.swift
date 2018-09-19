3a869c181f152cc612143bacf5fd2b09524298ec
Ron Diamond <rondiamond@users.noreply.github.com>
2018-09-19 00:04:17 -0700

    view, view controller - in progress

--- Reduxion-iOS/ViewController.swift
+++ Reduxion-iOS/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var operand1TextField: UITextField!
+    @IBAction func operand1EditingDidEnd(_ sender: Any) {
+        print("\n \(self) \(#function) line \(#line); NSDate = \(NSDate.init().timeIntervalSince1970)")
+    }
+    @IBOutlet weak var operand2TextField: UITextField!
+    @IBAction func operand2EditingDidEnd(_ sender: Any) {
+        print("\n \(self) \(#function) line \(#line); NSDate = \(NSDate.init().timeIntervalSince1970)")
+    }
+    @IBOutlet weak var calculationTypeSegmentedControl: UISegmentedControl!
+    @IBAction func buttonCalculateTapped(_ sender: Any) {
+        print("\n \(self) \(#function) line \(#line); NSDate = \(NSDate.init().timeIntervalSince1970)")
+    }
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
