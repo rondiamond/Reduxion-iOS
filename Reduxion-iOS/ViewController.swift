f92c83ccae53570a10e096ac10d2cbf39cf0b160
Ron Diamond <rondiamond@users.noreply.github.com>
2018-09-18 23:56:06 -0700

    creating view, view controller - in progress

---
+++ Reduxion-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reduxion-iOS
+//
+//  Created by Ron Diamond on 9/18/18.
+//  Copyright © 2018 Ron Diamond. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ View/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Reduxion-iOS
+//
+//  Created by Ron Diamond on 9/18/18.
+//  Copyright © 2018 Ron Diamond. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
