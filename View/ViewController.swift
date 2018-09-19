c10833b7c7f7ec07be195d0e551b417f60ffeb14
Ron Diamond <rondiamond@users.noreply.github.com>
2018-09-19 00:08:25 -0700

    view, view controller - in progress - now compiles

--- View/ViewController.swift
+++ View/ViewController.swift
@@ -10,21 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var operand1TextField: UITextField!
+    @IBAction func operand1EditingDidEnd(_ sender: Any) {
+        print("\n \(self) \(#function) line \(#line); NSDate = \(NSDate.init().timeIntervalSince1970)")
+    }
+    @IBOutlet weak var operand2TextField: UITextField!
+    @IBAction func operand2EditingDidEnd(_ sender: Any) {
+        print("\n \(self) \(#function) line \(#line); NSDate = \(NSDate.init().timeIntervalSince1970)")
+    }
+    @IBOutlet weak var calculationTypeSegmentedControl: UISegmentedControl!
+    @IBAction func buttonCalculateTapped(_ sender: Any) {
+        print("\n \(self) \(#function) line \(#line); NSDate = \(NSDate.init().timeIntervalSince1970)")
+    }
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
