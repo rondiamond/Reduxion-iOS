dac81ac6ee1c20d58e8788d75ff6b4d9ffd39039
Ron Diamond <rondiamond@users.noreply.github.com>
2018-09-19 01:40:14 -0700

    view controller calculation request - in progress

--- Reduxion-iOS sample app/ViewController.swift
+++ Reduxion-iOS sample app/ViewController.swift
@@ -12,19 +12,14 @@ class ViewController: UIViewController, AppStateSubscriber {
 
     var appStateSubscriberIdentifier: String = ""
 
+    // MARK: - IBOutlets
     @IBOutlet weak var operand1TextField: UITextField!
-    @IBAction func operand1EditingDidEnd(_ sender: Any) {
-        print("\n \(self) \(#function) line \(#line); NSDate = \(NSDate.init().timeIntervalSince1970)")
-    }
     @IBOutlet weak var operand2TextField: UITextField!
-    @IBAction func operand2EditingDidEnd(_ sender: Any) {
-        print("\n \(self) \(#function) line \(#line); NSDate = \(NSDate.init().timeIntervalSince1970)")
-    }
     @IBOutlet weak var calculationTypeSegmentedControl: UISegmentedControl!
-    @IBAction func buttonCalculateTapped(_ sender: Any) {
-        print("\n \(self) \(#function) line \(#line); NSDate = \(NSDate.init().timeIntervalSince1970)")
-    }
     @IBOutlet weak var resultLabel: UILabel!
+
+    
+    // MARK: - UIViewController lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,7 +29,47 @@ class ViewController: UIViewController, AppStateSubscriber {
     deinit {
         LogicCoordinator.sharedInstance.unsubscribe(self)
     }
+
     
+    // MARK: - IBActions
+    
+    @IBAction func operand1EditingDidEnd(_ sender: Any) {
+        print("\n \(self) \(#function) line \(#line); NSDate = \(NSDate.init().timeIntervalSince1970)")
+    }
+
+    @IBAction func operand2EditingDidEnd(_ sender: Any) {
+        print("\n \(self) \(#function) line \(#line); NSDate = \(NSDate.init().timeIntervalSince1970)")
+    }
+
+    @IBAction func buttonCalculateTapped(_ sender: Any) {
+        print("\n \(self) \(#function) line \(#line); NSDate = \(NSDate.init().timeIntervalSince1970)")
+        
+        var operand1: Float = 0
+        var operand2: Float = 0
+        if let operand1Text = operand1TextField.text {
+            operand1 = Float(operand1Text)!
+        }
+        if let operand2Text = operand2TextField.text {
+            operand2 = Float(operand2Text)!
+        }
+        
+        let calculationType: CalculationType
+        switch self.calculationTypeSegmentedControl.selectedSegmentIndex {
+        case 0:
+            calculationType = .addition
+        case 1:
+            calculationType = .subtraction
+        case 2:
+            calculationType = .multiplication
+        case 3:
+            calculationType = .division
+        default:
+            calculationType = .addition
+        }
+        
+        LogicCoordinator.sharedInstance.performAction(Action.performCalculation(operand1: operand1, operand2: operand2, calculationType: calculationType))
+    }
+
     
     // MARK: - AppState
     
@@ -49,4 +84,3 @@ class ViewController: UIViewController, AppStateSubscriber {
     }
 
 }
-
