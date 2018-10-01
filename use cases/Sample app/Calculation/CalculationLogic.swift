63bcfcb4f8abfb99387ab7761c3da8045c25e57b
Ron Diamond <rondiamond@users.noreply.github.com>
2018-09-30 23:31:32 -0700

    calculation history - fixed off-by-one error in rewriting history (that deleted current calculation, vs. appending after it)

--- use cases/Sample app/Calculation/CalculationLogic.swift
+++ use cases/Sample app/Calculation/CalculationLogic.swift
@@ -50,13 +50,19 @@ struct CalculationLogic: Logic {
     }
     
     private func removeUnusedFutureHistory(with state: AppState) {
+        print("\n \(self) \(#function) line \(#line); NSDate = \(NSDate.init().timeIntervalSince1970)")
+        print("... [before] historyCurrentIndex = \(String(describing: state.calculations.currentIndex))")
+        print("... [before] totalNumberOfHistoryCalculations = \(state.calculations.history.count)")
         if let historyCurrentIndex = state.calculations.currentIndex {
             let totalNumberOfHistoryCalculations = state.calculations.history.count
             if historyCurrentIndex < (totalNumberOfHistoryCalculations - 1) {
-                let subrangeToRemove = historyCurrentIndex..<totalNumberOfHistoryCalculations
+                let subrangeToRemove = (historyCurrentIndex+1)..<totalNumberOfHistoryCalculations
+                print("... subrangeToRemove = \(subrangeToRemove)")
                 state.calculations.history.removeSubrange(subrangeToRemove)
             }
         }
+        print("... [after] historyCurrentIndex = \(String(describing: state.calculations.currentIndex))")
+        print("... [after] totalNumberOfHistoryCalculations = \(state.calculations.history.count)")
     }
     
     
