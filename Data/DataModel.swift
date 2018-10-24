a0ed9dbfddbda5a78c2df1f8236ee47de72b0640
Ron Diamond <rondiamond@users.noreply.github.com>
2018-10-24 02:04:44 -0700

    Calculation: added NSCoder methods

--- Data/DataModel.swift
+++ Data/DataModel.swift
@@ -8,11 +8,30 @@ import Foundation
  -  A type of calculation (e.g., addition, subtraction, etc.)
  -  A timestamp of when the calculation was requested
  */
-struct Calculation {
+class Calculation: NSObject, NSCoding {
     var operand1: Float
     var operand2: Float
     var calculationType: CalculationType
     var result: Float?
+    
+    private let keyNameOperand1         = "operand1"
+    private let keyNameOperand2         = "operand2"
+    private let keyNameCalculationType  = "calculationType"
+    private let keyNameResult           = "result"
+
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(self.operand1,        forKey:keyNameOperand1)
+        aCoder.encode(self.operand2,        forKey:keyNameOperand2)
+        aCoder.encode(self.calculationType, forKey:keyNameCalculationType)
+        aCoder.encode(self.result,          forKey:keyNameResult)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        self.operand1           = aDecoder.decodeFloat(forKey: keyNameOperand1) as Float
+        self.operand2           = aDecoder.decodeFloat(forKey: keyNameOperand2) as Float
+        self.calculationType    = aDecoder.decodeObject(forKey: keyNameCalculationType) as? CalculationType ?? .addition   // TODO
+        self.result             = aDecoder.decodeFloat(forKey: keyNameResult) as Float
+    }
 }
 
 /**
